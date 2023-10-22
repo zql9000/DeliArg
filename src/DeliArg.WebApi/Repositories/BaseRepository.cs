c32d90fe49c7f697a2f151210f94dc1b18ceb9a2
Ezequiel Lopez <ezequiel2@gmail.com>
2023-10-22 19:35:13 +0200

    feat: base repository

---
+++ src/DeliArg.WebApi/Repositories/BaseRepository.cs
@@ -0,0 +1,62 @@
+﻿using DeliArg.WebApi.Data;
+using DeliArg.WebApi.Models;
+using DeliArg.WebApi.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace DeliArg.WebApi.Repositories;
+
+public abstract class BaseRepository<T> : IBaseRepository<T> where T : Base
+{
+    private readonly DeliArgDbContext context;
+    protected DbSet<T> Entities => context.Set<T>();
+
+
+    public BaseRepository(DeliArgDbContext context)
+    {
+        this.context = context;
+    }
+
+    public IQueryable<T> GetAll()
+    {
+        return Entities;
+    }
+
+    public async Task<T?> GetById(int id)
+    {
+        return await Entities.FirstOrDefaultAsync(e => e.Id == id);
+    }
+
+    public async Task<T> Insert(T entity)
+    {
+        EntityEntry<T> insertedValue = await Entities.AddAsync(entity);
+        return insertedValue.Entity;
+    }
+
+    public void Update(T entity)
+    {
+        Entities.Update(entity);
+    }
+
+    public async Task<bool> SoftDelete(int id)
+    {
+        T? entity = await GetById(id);
+
+        if (entity is null)
+            return false;
+
+        Update(entity);
+        return true;
+    }
+
+    public async Task<bool> HardDelete(int id)
+    {
+        T? entity = await GetById(id);
+
+        if (entity is null)
+            return false;
+
+        Entities.Remove(entity);
+        return true;
+    }
+}
---
+++ src/DeliArg.WebApi/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using DeliArg.WebApi.Models;
+
+namespace DeliArg.WebApi.Repositories.Interfaces;
+
+public interface IBaseRepository<T> where T : Base
+{
+    IQueryable<T> GetAll();
+    Task<T?> GetById(int id);
+    Task<T> Insert(T entity);
+    void Update(T entity);
+    Task<bool> SoftDelete(int id);
+    Task<bool> HardDelete(int id);
+}
