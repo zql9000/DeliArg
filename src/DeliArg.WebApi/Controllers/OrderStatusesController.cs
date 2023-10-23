495338ae6a3a4c8a1b51b9dbbb8b866294cd9f1b
Ezequiel Lopez <ezequiel2@gmail.com>
2023-10-23 23:48:32 +0200

    refactor: move order status crud to generic crud

--- src/DeliArg.WebApi/Controllers/OrderStatusesController.cs
+++ src/DeliArg.WebApi/Controllers/OrderStatusesController.cs
@@ -6,67 +6,8 @@ namespace DeliArg.WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class OrderStatusesController : ControllerBase
+public class OrderStatusesController
+    : BaseController<IOrderStatusService, OrderStatusRequestDto, OrderStatusResponseDto>
 {
-    private readonly IOrderStatusService orderStatusService;
-
-    public OrderStatusesController(IOrderStatusService orderStatusService)
-    {
-        this.orderStatusService = orderStatusService;
-    }
-
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<OrderStatusResponseDto>>> GetAll()
-    {
-        var orderStatuss = await orderStatusService.GetAll();
-
-        return Ok(orderStatuss);
-    }
-
-    [HttpGet("{id}")]
-    public async Task<ActionResult<OrderStatusResponseDto>> GetById(int id)
-    {
-        OrderStatusResponseDto? orderStatus = await orderStatusService.GetById(id);
-
-        if (orderStatus == null)
-        {
-            return NotFound();
-        }
-
-        return Ok(orderStatus);
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<OrderStatusResponseDto>> Create(OrderStatusRequestDto orderStatus)
-    {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-
-        OrderStatusResponseDto orderStatusCreated = await orderStatusService.Create(orderStatus);
-
-        return CreatedAtAction(nameof(GetById), new { id = orderStatusCreated.Id }, orderStatusCreated);
-    }
-
-    [HttpPut("{id}")]
-    public async Task<ActionResult> Update(int id, OrderStatusRequestDto orderStatus)
-    {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-
-        await orderStatusService.Update(id, orderStatus);
-
-        return NoContent();
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<ActionResult<bool>> Delete(int id)
-    {
-        bool result = await orderStatusService.Delete(id);
-
-        return Ok(result);
-    }
+    public OrderStatusesController(IOrderStatusService service) : base(service) { }
 }
